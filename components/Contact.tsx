74bda0e6aa82b12a4f8be5a274bc7dc10263442a
nourgajial26 <nourgajial@gmail.com>
2023-06-16 10:02:16 -0700

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -52,7 +52,7 @@ function Contact({}: Props) {
         autoPlay
         muted
         loop
-        style={{ opacity: 0.09 }}
+        style={{ opacity: 0.1 }}
     ></video>
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-300 text-2xl">Contact</h3>
 
